483abb28196e7101f888d5e3598a53e2bb49c4f7
Jonas Wiorek <jonas.wiorek@gmail.com>
2015-02-20 16:02:31 +0100

    Added barplot with barplot and ggplot

--- PA2_template.R
+++ PA2_template.R
@@ -6,11 +6,39 @@ eventFatalitiesInjuries <- summarize(group_by(stormData, EVTYPE),
                                      fatalities = sum(FATALITIES, na.rm=TRUE),
                                      injuries = sum(INJURIES, na.rm =TRUE))
 
-fatalitiesOrdered <- arrange(eventFatalitiesInjuries, desc(fatalities))
 
-qplot(EVTYPE, fatalities, data= fatalitiesOrdered)
+fatalitiesOrdered <- arrange(eventFatalitiesInjuries, desc(fatalities))
+fatalitiesOrderedFiltered <- fatalitiesOrdered[1:10,]
 
 injuriesOrdered <- arrange(eventFatalitiesInjuries, desc(injuries))
+injuriesOrderedFiltered <- injuriesOrdered[1:10,]
+
+ggplot(data=fatalitiesOrderedFiltered,aes(x=EVTYPE,y=fatalities)) +
+  geom_bar( stat="identity") +
+  scale_x_discrete(limits = fatalitiesOrderedFiltered$EVTYPE) +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=90))
+
+ggplot(data=injuriesOrderedFiltered,aes(x=EVTYPE,y=injuries)) +
+  geom_bar( stat="identity") +
+  scale_x_discrete(limits = injuriesOrderedFiltered$EVTYPE) +
+  theme(text = element_text(size=12), axis.text.x = element_text(angle=90))
+
+par(mar=c(7,4,4,2))
+barplot(fatalitiesOrderedFiltered$fatalities, names.arg = fatalitiesOrderedFiltered$EVTYPE, las =3, cex.names = 0.6)
+barplot(injuriesOrderedFiltered$injuries, names.arg = injuriesOrderedFiltered$EVTYPE, las =3, cex.names = 0.6)
+par(mar=c(5,4,4,2))
+
+
+
+
+
+
+
+
+
+
+qplot(x=EVTYPE, y=fatalities, data= fatalitiesOrderedFiltered, geom="bar")
+
 
 barplot(fatalitiesOrdered$fatalities[1:20])
 
